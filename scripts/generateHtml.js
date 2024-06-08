5f5cae4e0736acea371a0243d164fb695c7691c2
ThomasPictures <ThomasPictures@users.noreply.github.com>
2024-06-08 15:54:24 +0000

    deploy: 275706baf465d309d46a9dba005667ec3c0d156a

--- scripts/generateHtml.js
+++ scripts/generateHtml.js
@@ -32,7 +32,7 @@ function generateHtmlContent(horizontal, vertical) {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Thomas's Pictures CHANGE</title>
+  <title>Thomas's Pictures</title>
   <link rel="stylesheet" href="styles.css">
   <link rel="icon" href="icon.png">
 </head>
